479066524aebc5302d9bc5b69c53e1f60b5c96ec
Lexamenrf44 <alexander.stcstudent@gmail.com>
2022-03-29 21:40:18 +0300

    added configuration directory, created and configured TestBase.java

---
+++ src/test/java/com/tricentis/demowebshop/configuration/TestBase.java
@@ -0,0 +1,50 @@
+package com.tricentis.demowebshop.configuration;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import com.tricentis.demowebshop.helpers.Attach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class TestBase {
+
+    @BeforeAll
+    public static void browserOpeningConfiguration() {
+
+        Configuration.baseUrl = "http://demowebshop.tricentis.com/";
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "91");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+
+        /*
+        String remoteUrl = System.getProperty("remoteUrl");
+        String user = System.getProperty("user");
+        String password = System.getProperty("password");
+        Configuration.remote = "https://" + user + ":" + password + "@" + remoteUrl;
+         */
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @AfterEach
+    void addAttachments() {
+
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        closeWebDriver();
+
+    }
+
+}
