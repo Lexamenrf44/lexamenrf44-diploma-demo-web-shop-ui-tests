f692e8b8bde596494a805904e423115b4ae4bb9f
Lexamenrf44 <alexander.stcstudent@gmail.com>
2022-03-31 10:28:26 +0300

    refactored DemoWebShopTopMenuComputersTests.java, by renaming it into DemoWebShopComputersTests.java

--- src/test/java/com/tricentis/demowebshop/tests/DemoWebShopComputersTests.java
+++ src/test/java/com/tricentis/demowebshop/tests/DemoWebShopComputersTests.java
@@ -9,7 +9,7 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
-public class DemoWebShopTopMenuComputersTests extends TestBase {
+public class DemoWebShopComputersTests extends TestBase {
 
     Computers computers = new Computers();
 
