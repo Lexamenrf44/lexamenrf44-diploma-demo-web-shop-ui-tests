164d4397a6d114fa942e08f083445c3da0e776ac
Lexamenrf44 <alexander.stcstudent@gmail.com>
2022-04-02 00:00:16 +0300

    added 'leftsidemenu' directory into 'pages' directory, created ComputersCategory.java in the root directory, configuring ComputersCategory.java, added checkLeftMenuComputersButton test method

---
+++ src/test/java/com/tricentis/demowebshop/pages/leftsidemenu/ComputersCategory.java
@@ -0,0 +1,16 @@
+package com.tricentis.demowebshop.pages.leftsidemenu;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
+public class ComputersCategory {
+
+    public ComputersCategory checkLeftMenuComputersButton() {
+        $(".block-category-navigation [href*='/computers']").should(appear);
+        $(".block-category-navigation [href*='/computers']").shouldBe(visible);
+
+        return this;
+    }
+
+}
