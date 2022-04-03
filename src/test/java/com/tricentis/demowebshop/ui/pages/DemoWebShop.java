c31ae461c13a1095e93ca04d8fc1e8936bfd76b8
Lexamenrf44 <alexander.stcstudent@gmail.com>
2022-04-03 19:33:00 +0300

    configuring project infrastructure, added DemoWebShop.java to the "pages" directory, added openDemoWebShop test method

---
+++ src/test/java/com/tricentis/demowebshop/ui/pages/DemoWebShop.java
@@ -0,0 +1,14 @@
+package com.tricentis.demowebshop.ui.pages;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class DemoWebShop {
+
+    public DemoWebShop openDemoWebShop() {
+
+        open((""));
+
+        return this;
+    }
+
+}
