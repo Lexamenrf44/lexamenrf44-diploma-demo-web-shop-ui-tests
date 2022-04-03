9679039409e43e7c1ceaca606339311173269a02
Lexamenrf44 <alexander.stcstudent@gmail.com>
2022-04-03 20:11:32 +0300

    refactoring ComputersSection.java, added test methods: checkComputersSectionPage, checkComputersSectionTitle and checkComputersSectionSubCategories from TopMenuComputers.java

--- src/test/java/com/tricentis/demowebshop/ui/pages/sections/ComputersSection.java
+++ src/test/java/com/tricentis/demowebshop/ui/pages/sections/ComputersSection.java
@@ -1,4 +1,47 @@
 package com.tricentis.demowebshop.ui.pages.sections;
 
+import com.tricentis.demowebshop.ui.pages.topmenu.TopMenuComputers;
+
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
 public class ComputersSection {
+
+    public ComputersSection goToComputersSection() {
+
+        $(".top-menu [href*='/computers']").click();
+
+        return this;
+    }
+
+    public ComputersSection checkComputersSectionPage() {
+        $("[class='page category-page']").should(appear);
+        $("[class='page category-page']").shouldBe(visible);
+
+
+        return this;
+    }
+
+    public ComputersSection checkComputersSectionTitle() {
+        $("[class='page-title']").should(appear);
+        $("[class='page-title']").shouldBe(visible);
+        $("[class='page-title']").shouldHave(text("Computers"));
+
+        return this;
+    }
+
+    public ComputersSection checkComputersSectionSubCategories() {
+        $("[class='sub-category-grid']").should(appear);
+        $("[class='sub-category-grid']").should(visible);
+        $$(".item-box").shouldHave(size(3));
+        $(".page-body .sub-category-grid .item-box .title [href*='/desktops']").shouldHave(text("Desktops"));
+        $(".page-body .sub-category-grid .item-box .title [href*='/notebooks']").shouldHave(text("Notebooks"));
+        $(".page-body .sub-category-grid .item-box .title [href*='/accessories']").shouldHave(text("Accessories"));
+
+        return this;
+    }
+
 }
