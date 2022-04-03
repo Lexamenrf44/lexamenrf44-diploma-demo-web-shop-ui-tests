6759b86d53f908ae8fa22281b6f2e3d4492f01d5
Lexamenrf44 <alexander.stcstudent@gmail.com>
2022-04-03 19:43:58 +0300

    configuring project infrastructure, moved TopMenuComputers.java to the "topmenu" directory

--- src/test/java/com/tricentis/demowebshop/ui/pages/topmenu/TopMenuComputers.java
+++ src/test/java/com/tricentis/demowebshop/ui/pages/topmenu/TopMenuComputers.java
@@ -5,16 +5,9 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class Computers {
+public class TopMenuComputers {
 
-    public Computers openDemoWebShop() {
-
-        open((""));
-
-        return this;
-    }
-
-    public Computers checkTopMenuComputersHoverButton() {
+    public TopMenuComputers checkTopMenuComputersHoverButton() {
         $(".top-menu [href*='/computers']").hover();
         $(withText("Desktops")).should(appear);
         $(withText("Notebooks")).should(appear);
@@ -23,14 +16,14 @@ public class Computers {
         return this;
     }
 
-    public Computers goToComputersSection() {
+    public TopMenuComputers goToComputersSection() {
 
         $(".top-menu [href*='/computers']").click();
 
         return this;
     }
 
-    public Computers checkComputersSectionPage() {
+    public TopMenuComputers checkComputersSectionPage() {
         $("[class='page category-page']").should(appear);
         $("[class='page category-page']").shouldBe(visible);
 
@@ -38,7 +31,7 @@ public class Computers {
         return this;
     }
 
-    public Computers checkComputersSectionTitle() {
+    public TopMenuComputers checkComputersSectionTitle() {
         $("[class='page-title']").should(appear);
         $("[class='page-title']").shouldBe(visible);
         $("[class='page-title']").shouldHave(text("Computers"));
@@ -46,7 +39,7 @@ public class Computers {
         return this;
     }
 
-    public Computers checkComputersSectionSubCategories() {
+    public TopMenuComputers checkComputersSectionSubCategories() {
         $("[class='sub-category-grid']").should(appear);
         $("[class='sub-category-grid']").should(visible);
         $$(".item-box").shouldHave(size(3));
@@ -57,14 +50,14 @@ public class Computers {
         return this;
     }
 
-    public Computers goToDesktopsBrowsePage() {
+    public TopMenuComputers goToDesktopsBrowsePage() {
 
         $("[alt='Picture for category Desktops']").click();
 
         return this;
     }
 
-    public Computers checkDesktopsSection() {
+    public TopMenuComputers checkDesktopsSection() {
         $("[class='page-title']").should(appear);
         $("[class='page-title']").shouldBe(visible);
         $("[class='page-title']").shouldHave(text("Desktops"));
@@ -72,14 +65,14 @@ public class Computers {
         return this;
     }
 
-    public Computers goToNotebooksBrowsePage() {
+    public TopMenuComputers goToNotebooksBrowsePage() {
 
         $("[alt='Picture for category Notebooks']").click();
 
         return this;
     }
 
-    public Computers checkNotebooksSection() {
+    public TopMenuComputers checkNotebooksSection() {
         $("[class='page-title']").should(appear);
         $("[class='page-title']").shouldBe(visible);
         $("[class='page-title']").shouldHave(text("Notebooks"));
@@ -87,14 +80,14 @@ public class Computers {
         return this;
     }
 
-    public Computers goToAccessoriesBrowsePage() {
+    public TopMenuComputers goToAccessoriesBrowsePage() {
 
         $("[alt='Picture for category Accessories']").click();
 
         return this;
     }
 
-    public Computers checkAccessoriesSection() {
+    public TopMenuComputers checkAccessoriesSection() {
         $("[class='page-title']").should(appear);
         $("[class='page-title']").shouldBe(visible);
         $("[class='page-title']").shouldHave(text("Accessories"));
