c242fd8ef8053baa618a6ed1e21baf792ad0768d
Lexamenrf44 <alexander.stcstudent@gmail.com>
2022-04-04 13:29:48 +0300

    configuring RegistrationForm.java, added checkRegistrationPage test method

--- src/test/java/com/tricentis/demowebshop/ui/pages/RegistrationForm.java
+++ src/test/java/com/tricentis/demowebshop/ui/pages/RegistrationForm.java
@@ -1,5 +1,6 @@
 package com.tricentis.demowebshop.ui.pages;
 
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 
 public class RegistrationForm {
@@ -10,4 +11,16 @@ public class RegistrationForm {
 
         return this;
     }
+
+    public RegistrationForm checkRegistrationPage() {
+
+        $(".registration-page").should(appear);
+        $(".registration-page").shouldBe(visible);
+        $(".registration-page .page-title").shouldHave(text("Register"));
+        $(".registration-page .page-body").should(appear);
+        $(".registration-page .page-body").shouldBe(visible);
+
+        return this;
+    }
+
 }
