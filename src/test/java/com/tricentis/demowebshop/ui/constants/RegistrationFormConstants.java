be356b2dd91463e3f04aed553be4dbe9210782c6
Lexamenrf44 <alexander.stcstudent@gmail.com>
2022-04-05 22:58:00 +0300

    configuring RegistrationFormConstants.java, added public static class 'Gender' for DemoWebShopRegistrationTests.java

--- src/test/java/com/tricentis/demowebshop/ui/constants/RegistrationFormConstants.java
+++ src/test/java/com/tricentis/demowebshop/ui/constants/RegistrationFormConstants.java
@@ -1,4 +1,12 @@
 package com.tricentis.demowebshop.ui.constants;
 
 public class RegistrationFormConstants {
+
+    public static class Gender {
+
+        public static final String MALE = "Male";
+        public static final String FEMALE = "Female";
+
+    }
+
 }
