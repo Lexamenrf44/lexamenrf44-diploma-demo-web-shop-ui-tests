b1d759626e051ce5aad2f9a3f2865f89fc61fe9c
Lexamenrf44 <alexander.stcstudent@gmail.com>
2022-04-07 23:59:49 +0300

    configuring DemoWebShopRegistrationTests.java added registrationGenderSelectTest test-case

--- src/test/java/com/tricentis/demowebshop/ui/tests/DemoWebShopRegistrationTests.java
+++ src/test/java/com/tricentis/demowebshop/ui/tests/DemoWebShopRegistrationTests.java
@@ -8,9 +8,14 @@ import io.qameta.allure.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static com.tricentis.demowebshop.ui.constants.RegistrationFormConstants.Gender.FEMALE;
+import static com.tricentis.demowebshop.ui.constants.RegistrationFormConstants.Gender.MALE;
 import static io.qameta.allure.Allure.step;
 
 public class DemoWebShopRegistrationTests extends TestBase {
+
+    DemoWebShop demoWebShop = new DemoWebShop();
+    RegistrationForm registrationForm = new RegistrationForm();
 
     Faker faker = new Faker();
     String firstName = faker.name().firstName();
@@ -19,18 +24,15 @@ public class DemoWebShopRegistrationTests extends TestBase {
     String password = faker.internet().password();
     String confirmPassword = faker.internet().password();
 
-    DemoWebShop demoWebShop = new DemoWebShop();
-    RegistrationForm registrationForm = new RegistrationForm();
-
     @Owner("lexamenrf44")
-    @Feature("Computers")
+    @Feature("RegistrationForm")
     @Story("Реализовать страницу регистрации в магазине Demo Web Shop")
     @DisplayName("Тестирование отображения страницы регистрации магазина")
     @Severity(SeverityLevel.NORMAL)
     @Test
     void registrationPageTest() {
 
-        step("Открыть сайт Demo Web Shop", () -> {
+        step("Открыть Demo Web Shop", () -> {
             demoWebShop.openDemoWebShop();
         });
 
@@ -61,6 +63,35 @@ public class DemoWebShopRegistrationTests extends TestBase {
 
         step("Отправить форму регистрации пользователя", () -> {
             registrationForm.submitUserRegistrationForm();
+        });
+    }
+
+    @Owner("lexamenrf44")
+    @Feature("Computers")
+    @Story("Реализовать страницу регистрации в магазине Demo Web Shop")
+    @DisplayName("Тестирование отображения страницы регистрации магазина")
+    @Severity(SeverityLevel.NORMAL)
+    @Test
+    void registrationGenderSelectTest() {
+
+        step("Открыть сайт Demo Web Shop", () -> {
+            demoWebShop.openDemoWebShop();
+        });
+
+        step("Переход на форму регистрации Demo Web Shop", () -> {
+            registrationForm.goToRegistrationPage();
+        });
+
+        step("Выбрать пол", () -> {
+            registrationForm.selectUserGender(FEMALE);
+        });
+
+        step("Отправить форму регистрации пользователя", () -> {
+            registrationForm.submitUserRegistrationForm();
+        });
+
+        step("Проверить поле Имя", () -> {
+            registrationForm.checkFirstNameField();
         });
     }
 
