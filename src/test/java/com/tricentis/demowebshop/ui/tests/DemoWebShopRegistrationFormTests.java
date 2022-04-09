eeaec705c7ed3c4d84fb1ed6cd3935af83777871
Lexamenrf44 <alexander.stcstudent@gmail.com>
2022-04-09 21:57:21 +0300

    configuring DemoWebShopRegistrationFormTests.java, added and configured tests

--- src/test/java/com/tricentis/demowebshop/ui/tests/DemoWebShopRegistrationFormTests.java
+++ src/test/java/com/tricentis/demowebshop/ui/tests/DemoWebShopRegistrationFormTests.java
@@ -23,12 +23,12 @@ public class DemoWebShopRegistrationFormTests extends TestBase {
     String password = faker.internet().password();
     String confirmPassword = faker.internet().password();
 
+    @Test
     @Owner("lexamenrf44")
     @Feature("RegistrationForm")
-    @Story("Реализовать страницу регистрации в магазине Demo Web Shop")
-    @DisplayName("Тестирование отображения страницы регистрации магазина")
+    @Story("Реализовать страницу регистрации новых пользователей в магазине Demo Web Shop")
     @Severity(SeverityLevel.NORMAL)
-    @Test
+    @DisplayName("Тестирование отображения страницы регистрации магазина")
     void registrationPageTest() {
 
         step("Открыть Demo Web Shop", () -> {
@@ -44,13 +44,13 @@ public class DemoWebShopRegistrationFormTests extends TestBase {
         });
     }
 
+    @Test
     @Owner("lexamenrf44")
     @Feature("Computers")
-    @Story("Реализовать страницу регистрации в магазине Demo Web Shop")
-    @DisplayName("Тестирование отображения страницы регистрации магазина")
+    @Story("Реализовать функционал регистрации новых пользователей в магазине Demo Web Shop")
     @Severity(SeverityLevel.NORMAL)
-    @Test
-    void registrationEmptyPageSubmitTest() {
+    @DisplayName("Тестирование отправки незаполненной формы регистрации нового пользователя")
+    void registrationEmptyFormSubmitTest() {
 
         step("Открыть сайт Demo Web Shop", () -> {
             mainPage.openDemoWebShop();
@@ -63,15 +63,23 @@ public class DemoWebShopRegistrationFormTests extends TestBase {
         step("Отправить форму регистрации пользователя", () -> {
             registrationFormPage.submitUserRegistrationForm();
         });
+
+        step("Проверка заполненной формы", () -> {
+            registrationFormPage.checkFirstNameField()
+                                .checkLastNameField()
+                                .checkEmailField()
+                                .checkPasswordField()
+                                .checkConfirmPasswordField();
+        });
     }
 
+    @Test
     @Owner("lexamenrf44")
     @Feature("Computers")
-    @Story("Реализовать страницу регистрации в магазине Demo Web Shop")
-    @DisplayName("Тестирование отображения страницы регистрации магазина")
+    @Story("Реализовать функционал регистрации новых пользователей в магазине Demo Web Shop")
     @Severity(SeverityLevel.NORMAL)
-    @Test
-    void registrationGenderSelectTest() {
+    @DisplayName("Тестирование отправки полностью заполненной формы регистрации нового пользователя")
+    void registrationFilledFormSubmitTest() {
 
         step("Открыть сайт Demo Web Shop", () -> {
             mainPage.openDemoWebShop();
@@ -85,13 +93,243 @@ public class DemoWebShopRegistrationFormTests extends TestBase {
             registrationFormPage.selectUserGender(FEMALE);
         });
 
+        step("Заполнить поле First Name", () -> {
+            registrationFormPage.enterFirstName(firstName);
+        });
+
+        step("Заполнить поле Last Name", () -> {
+            registrationFormPage.enterLastName(lastName);
+        });
+
+        step("Заполнить поле Email", () -> {
+            registrationFormPage.enterUserEmail(email);
+        });
+
+        step("Заполнить поле Password", () -> {
+            registrationFormPage.enterPassword(password);
+        });
+
+        step("Заполнить поле Confirm Password", () -> {
+            registrationFormPage.enterConfirmPassword(password);
+        });
+
         step("Отправить форму регистрации пользователя", () -> {
             registrationFormPage.submitUserRegistrationForm();
         });
 
-        step("Проверить поле Имя", () -> {
-            registrationFormPage.checkFirstNameField();
+        step("Проверка успешной регистрации пользователя", () -> {
+            registrationFormPage.checkSuccessfulRegistration();
         });
+    }
+
+    @Test
+    @Owner("lexamenrf44")
+    @Feature("Computers")
+    @Story("Реализовать функционал регистрации новых пользователей в магазине Demo Web Shop")
+    @Severity(SeverityLevel.NORMAL)
+    @DisplayName("Тестирование отправки формы регистрации нового пользователя с заполненным полем First Name")
+    void registrationFirstNameFieldFilledOnlyTest() {
+
+        step("Открыть сайт Demo Web Shop", () -> {
+            mainPage.openDemoWebShop();
+        });
+
+        step("Переход на форму регистрации Demo Web Shop", () -> {
+            registrationFormPage.goToRegistrationPage();
+        });
+
+        step("Заполнить поле First Name", () -> {
+            registrationFormPage.enterFirstName(firstName);
+        });
+
+        step("Отправить форму регистрации пользователя", () -> {
+            registrationFormPage.submitUserRegistrationForm();
+        });
+
+        step("Проверка заполненной формы", () -> {
+            registrationFormPage.checkLastNameField()
+                    .checkEmailField()
+                    .checkPasswordField()
+                    .checkConfirmPasswordField();
+        });
+    }
+
+    @Test
+    @Owner("lexamenrf44")
+    @Feature("Computers")
+    @Story("Реализовать функционал регистрации новых пользователей в магазине Demo Web Shop")
+    @Severity(SeverityLevel.NORMAL)
+    @DisplayName("Тестирование отправки формы регистрации нового пользователя с заполненным полем Last Name")
+    void registrationLastNameFieldFilledOnlyTest() {
+
+        step("Открыть сайт Demo Web Shop", () -> {
+            mainPage.openDemoWebShop();
+        });
+
+        step("Переход на форму регистрации Demo Web Shop", () -> {
+            registrationFormPage.goToRegistrationPage();
+        });
+
+        step("Заполнить поле Last Name", () -> {
+            registrationFormPage.enterLastName(lastName);
+        });
+
+        step("Отправить форму регистрации пользователя", () -> {
+            registrationFormPage.submitUserRegistrationForm();
+        });
+
+        step("Проверка заполненной формы", () -> {
+            registrationFormPage.checkFirstNameField()
+                    .checkEmailField()
+                    .checkPasswordField()
+                    .checkConfirmPasswordField();
+        });
+    }
+
+    @Test
+    @Owner("lexamenrf44")
+    @Feature("Computers")
+    @Story("Реализовать функционал регистрации новых пользователей в магазине Demo Web Shop")
+    @Severity(SeverityLevel.NORMAL)
+    @DisplayName("Тестирование отправки формы регистрации нового пользователя с заполненным полем Email")
+    void registrationEmailFieldFilledOnlyTest() {
+
+        step("Открыть сайт Demo Web Shop", () -> {
+            mainPage.openDemoWebShop();
+        });
+
+        step("Переход на форму регистрации Demo Web Shop", () -> {
+            registrationFormPage.goToRegistrationPage();
+        });
+
+        step("Заполнить поле Email", () -> {
+            registrationFormPage.enterUserEmail(email);
+        });
+
+        step("Отправить форму регистрации пользователя", () -> {
+            registrationFormPage.submitUserRegistrationForm();
+        });
+
+        step("Проверка заполненной формы", () -> {
+            registrationFormPage.checkFirstNameField()
+                    .checkLastNameField()
+                    .checkPasswordField()
+                    .checkConfirmPasswordField();
+        });
+    }
+
+    @Test
+    @Owner("lexamenrf44")
+    @Feature("Computers")
+    @Story("Реализовать функционал регистрации новых пользователей в магазине Demo Web Shop")
+    @Severity(SeverityLevel.NORMAL)
+    @DisplayName("Тестирование отправки формы регистрации нового пользователя с заполненным полем Password")
+    void registrationPasswordFieldFilledOnlyTest() {
+
+        step("Открыть сайт Demo Web Shop", () -> {
+            mainPage.openDemoWebShop();
+        });
+
+        step("Переход на форму регистрации Demo Web Shop", () -> {
+            registrationFormPage.goToRegistrationPage();
+        });
+
+        step("Заполнить поле Email", () -> {
+            registrationFormPage.enterPassword(password);
+        });
+
+        step("Отправить форму регистрации пользователя", () -> {
+            registrationFormPage.submitUserRegistrationForm();
+        });
+
+        step("Проверка заполненной формы", () -> {
+            registrationFormPage.checkFirstNameField()
+                    .checkLastNameField()
+                    .checkEmailField()
+                    .checkConfirmPasswordField();
+        });
+    }
+
+    @Test
+    @Owner("lexamenrf44")
+    @Feature("Computers")
+    @Story("Реализовать функционал регистрации новых пользователей в магазине Demo Web Shop")
+    @Severity(SeverityLevel.NORMAL)
+    @DisplayName("Тестирование отправки формы регистрации нового пользователя с заполненным полем Confirm Password")
+    void registrationConfirmPasswordFieldFilledOnlyTest() {
+
+        step("Открыть сайт Demo Web Shop", () -> {
+            mainPage.openDemoWebShop();
+        });
+
+        step("Переход на форму регистрации Demo Web Shop", () -> {
+            registrationFormPage.goToRegistrationPage();
+        });
+
+        step("Заполнить поле Email", () -> {
+            registrationFormPage.enterConfirmPassword(password);
+        });
+
+        step("Отправить форму регистрации пользователя", () -> {
+            registrationFormPage.submitUserRegistrationForm();
+        });
+
+        step("Проверка заполненной формы", () -> {
+            registrationFormPage.checkFirstNameField()
+                    .checkLastNameField()
+                    .checkEmailField()
+                    .checkPasswordField();
+        });
+    }
+
+    @Test
+    @Owner("lexamenrf44")
+    @Feature("Computers")
+    @Story("Реализовать функционал регистрации новых пользователей в магазине Demo Web Shop")
+    @Severity(SeverityLevel.NORMAL)
+    @DisplayName("Тестирование отправки заполненной формы регистрации нового пользователя с разными паролями")
+    void registrationDifferentPasswordsFilledTest() {
+
+        step("Открыть сайт Demo Web Shop", () -> {
+            mainPage.openDemoWebShop();
+        });
+
+        step("Переход на форму регистрации Demo Web Shop", () -> {
+            registrationFormPage.goToRegistrationPage();
+        });
+
+        step("Выбрать пол", () -> {
+            registrationFormPage.selectUserGender(FEMALE);
+        });
+
+        step("Заполнить поле First Name", () -> {
+            registrationFormPage.enterFirstName(firstName);
+        });
+
+        step("Заполнить поле Last Name", () -> {
+            registrationFormPage.enterLastName(lastName);
+        });
+
+        step("Заполнить поле Email", () -> {
+            registrationFormPage.enterUserEmail(email);
+        });
+
+        step("Заполнить поле Password", () -> {
+            registrationFormPage.enterPassword(password);
+        });
+
+        step("Заполнить поле Confirm Password", () -> {
+            registrationFormPage.enterDifferentConfirmPassword(confirmPassword);
+        });
+
+        step("Отправить форму регистрации пользователя", () -> {
+            registrationFormPage.submitUserRegistrationForm();
+        });
+
+        step("Проверка заполненной формы", () -> {
+            registrationFormPage.checkDifferentConfirmPasswordField();
+        });
+
     }
 
 }
