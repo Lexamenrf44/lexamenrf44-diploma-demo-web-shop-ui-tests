45102ff186b0604f4085693d2ee23c34a1dda580
Lexamenrf44 <alexander.stcstudent@gmail.com>
2022-04-12 23:59:40 +0300

    configuring project infrastructure, added and configured Credentials.java in the configuration root directory

---
+++ src/test/java/com/tricentis/demowebshop/ui/configuration/Credentials.java
@@ -0,0 +1,7 @@
+package com.tricentis.demowebshop.ui.configuration;
+
+import org.aeonbits.owner.ConfigFactory;
+
+public class Credentials {
+    public static CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
+}
